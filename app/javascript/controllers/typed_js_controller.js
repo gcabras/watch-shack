a609cdc20cfa990666df323df03a5ab139898247
Grazia Cabras <graziacabras.gcabras@gmail.com>
2022-11-27 21:19:43 +0100

    Few changes

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -5,9 +5,9 @@ import Typed from "typed.js"
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ["No more time wasted browsing", "Randomly", "Pick up suggestions", "Have a perfect Binge Night"],
-      typeSpeed: 50,
-      loop: true
+      strings: ["No more time wasted browsing", "Pick up suggestions", "Have a perfect Binge Night"],
+      typeSpeed: 65,
+      loop: false
     })
   }
 }
