d56ed64c2da4b7620a897f64f200d0aabd61b617
Grazia Cabras <graziacabras.gcabras@gmail.com>
2022-11-27 20:23:49 +0100

    add typed.js to homepage

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,8 +7,8 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-import ModalController from "./modal_controller"
-application.register("modal", ModalController)
-
 import TomSelectController from "./tom_select_controller"
 application.register("tom-select", TomSelectController)
+
+import TypedJsController from "./typed_js_controller"
+application.register("typed-js", TypedJsController)
---
+++ app/javascript/controllers/typed_js_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+import Typed from "typed.js"
+
+// Connects to data-controller="typed-js"
+export default class extends Controller {
+  connect() {
+    new Typed(this.element, {
+      strings: ["No more time wasted browsing", "Randomly", "Pick up suggestions", "Have a perfect Binge Night"],
+      typeSpeed: 50,
+      loop: true
+    })
+  }
+}
